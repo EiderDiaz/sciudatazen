a081a6edebb56dc92a3c2100b42c5aa40b763471
Edson Paul <73849076+EdsonMorfin@users.noreply.github.com>
2021-07-31 17:25:09 -0500

    plot model

---
+++ Plots/plot.js
@@ -0,0 +1,61 @@
+var data = {
+  labels: ['January', 'February', 'March'],
+  
+  datasets: [
+      {
+      fillColor: "rgba(220,220,220,0.2)",
+      strokeColor: "rgba(220,220,220,1)",
+      pointColor: "rgba(220,220,220,1)",
+      pointStrokeColor: "#fff",
+      pointHighlightFill: "#fff",
+      pointHighlightStroke: "rgba(220,220,220,1)",
+      data: [30,120,90]
+      },
+      {
+      fillColor: "rgba(100,220,220,0.7)",
+      strokeColor: "rgba(220,220,220,1)",
+      pointColor: "rgba(220,220,220,1)",
+      pointStrokeColor: "#fff",
+      pointHighlightFill: "#fff",
+      pointHighlightStroke: "rgba(220,220,220,1)",
+      data: [10,70,110]
+      }
+  ]
+  };
+
+var data1 = {
+  labels: ['March', 'Apr', 'May'],
+  
+  datasets: [
+      {
+      fillColor: "rgba(220,220,220,0.2)",
+      strokeColor: "rgba(220,220,220,1)",
+      pointColor: "rgba(220,220,220,1)",
+      pointStrokeColor: "#fff",
+      pointHighlightFill: "#fff",
+      pointHighlightStroke: "rgba(220,220,220,1)",
+      data: [50,100,140]
+      },
+      {
+      fillColor: "rgba(100,220,220,0.7)",
+      strokeColor: "rgba(220,220,220,1)",
+      pointColor: "rgba(220,220,220,1)",
+      pointStrokeColor: "#fff",
+      pointHighlightFill: "#fff",
+      pointHighlightStroke: "rgba(220,220,220,1)",
+      data: [40,70,200]
+      }
+  ]
+  };
+
+var context = document.querySelector('#graph').getContext('2d');
+new Chart(context).Line(data);
+  
+$("#btn1").on("click", function() {
+   var context1 = document.querySelector('#graph').getContext('2d');
+  new Chart(context1).Line(data);
+});
+$("#btn2").on("click", function() {
+  var context2 = document.querySelector('#graph').getContext('2d');
+  new Chart(context2).Line(data1);
+});
